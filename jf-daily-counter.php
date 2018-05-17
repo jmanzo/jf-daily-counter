6ba7c26804d77d02f6b7e39870c65ec6a4a200d8
Jean Manzo <jmanzosistemas@gmail.com>
2018-05-17 13:23:34 -0500

    initial commit

---
+++ jf-daily-counter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+Plugin Name: Jayfencing Daily Counter
+Plugin URI: https://jayfencing.com/
+Description: Daily counter for jayfencing site
+Author: Alex Leuchner - Jean Manzo
+Version: 0.1
+Author URI: https://about.me/jeanmanzo
+*/
+
